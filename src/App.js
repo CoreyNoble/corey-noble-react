a662c13e6aa13b79f8ea6cd0b05bc1c53e2c4d15
Corey Noble <coreynoble@hotmail.ca>
2020-03-21 00:02:12 -0400

    Init - Copying over all pre-existing assets (img, fonts, pdf, sounds, video), .gitignore *.php

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Corey Noble</h1>
-        <h2>Web Developer</h2>
+        <h2>Web Application Developer</h2>
       </header>
     </div>
   );
