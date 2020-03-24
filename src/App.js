8a7b3f24a87b644dfab12edbc343469061525175
Corey Noble <coreynoble@hotmail.ca>
2020-03-23 22:54:17 -0400

    General Setup - Media Queries, Index Banner, react-dom, npm 'three', Foundation, AOS.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+
 import './app.scss';
+
 import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
 import BackToTop from './components/BackToTop/BackToTop';
 
 import Section from './containers/Section/Section';
 
+import IndexBanner from './components/IndexBanner/IndexBanner';
 import AboutIntro from './components/AboutIntro/AboutIntro';
 import Button from './components/Button/Button';
 
@@ -16,12 +19,90 @@ function App() {
       <Navigation />
       <BackToTop />
 
+      <IndexBanner />
+
       <AboutIntro />
       <Button />
 
       <Section background="blue">
         <h2>Skill Overview</h2>
         <h3 className="h4 index-header">A brief overview of what I bring to the&nbsp;table.</h3>
+
+        <div className="skill-overview row" data-equalizer>
+          <div className="services-promotion small-12 medium-12 large-4 columns" data-aos="fade-up" data-aos-delay="100"
+            data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/responsive-icon.png" alt="" role="presentation" />
+            <h4>Responsive Development</h4>
+            <p>I can ensure your website adapts to use different sizing and layouts depending on the resolution of the device
+              it is viewed on. This way your website looks great on anything your visitors may be&nbsp;using.</p>
+          </div>
+          <div className="services-promotion small-12 medium-12 large-4 columns end" data-aos="fade-up" data-aos-delay="100"
+            data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/seo-icon.png" alt="" role="presentation" />
+            <h4>Search and Accessibility</h4>
+            <p>I have experience in implementing the latest Search Engine Optimisation <a
+                href="{{root}}blog/search-engine-optimisation.html"
+                title="Learn more about Search Engine Optimisation">(SEO)</a> techniques, and ensuring websites meet the Web
+              Content Accessibility Guidelines <a href="{{root}}blog/web-content-accessibility-guidelines.html"
+                title="Learn more about the Web Content Accessibility Guidelines">(WCAG&nbsp;2.0)</a>.</p>
+          </div>
+          <div className="services-promotion small-12 medium-12 large-4 columns end" data-aos="fade-up" data-aos-delay="100"
+            data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/custom-functionality-icon.png" alt="" role="presentation" />
+            <h4>Custom Functionality</h4>
+            <p>If you need a custom tool or feature built for your site, let's discuss it. In most cases I should be able to
+              accommodate your&nbsp;needs.</p>
+          </div>
+        </div>
+
+        <div className="skill-overview row" data-equalizer>
+          <div className="services-promotion small-12 medium-12 large-4 columns" data-aos="fade-up" data-aos-delay="100"
+            data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/a-b-icon.png" alt="" role="presentation" />
+            <h4>Analytics and A/B Testing</h4>
+            <p>I have experience tracking user analytics, and creating targeted test campaigns. This enables you to cater to
+              segmented audiences and improve your user retention over time via. A/B&nbsp;testing.</p>
+          </div>
+          <div className="services-promotion small-12 medium-12 large-4 columns end" data-aos="fade-up" data-aos-delay="100"
+            data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/optimized-code-icon.png" alt="" role="presentation" />
+            <h4>Clean and Efficient Code</h4>
+            <p>I always optimize the code I write to ensure minimal load times. My code is written in an understandable,
+              programmatic
+              format so it is easy for any web developer to read and&nbsp;manipulate.</p>
+          </div>
+          <div className="services-promotion no-bottom-space-desktop small-12 medium-12 large-4 columns" data-aos="fade-up"
+            data-aos-delay="100" data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/cms-icon.png" alt="" role="presentation" />
+            <h4>Content Management Systems</h4>
+            <p>If you want to be your own web master, I can build your website with a content management system. This will
+              empower you to make the changes you want to make, on your own&nbsp;time.</p>
+          </div>
+        </div>
+
+        <div className="skill-overview row" data-equalizer>
+          <div className="services-promotion small-12 medium-12 large-4 columns" data-aos="fade-up" data-aos-delay="100"
+            data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/current-technologies-icon.png" alt="" role="presentation" />
+            <h4>Latest and Greatest</h4>
+            <p>I always build websites with the most current technologies in mind. I will keep your website
+              up&#8209;to&#8209;date with all of the latest and greatest&nbsp;trends.</p>
+          </div>
+          <div className="services-promotion no-bottom-space-desktop small-12 medium-12 large-4 columns" data-aos="fade-up"
+            data-aos-delay="100" data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/design-icon.png" alt="" role="presentation" />
+            <h4>Website Design and Branding</h4>
+            <p>I have a thorough understanding of both digital and print&nbsp;media. Let me know if you need a new design for
+              your website, new branding, or promotional&nbsp;artwork.</p>
+          </div>
+          <div className="services-promotion no-bottom-space small-12 medium-12 large-4 columns" data-aos="fade-up"
+            data-aos-delay="100" data-equalizer-watch>
+            <img src="{{root}}assets/img/icons/email-icon.png" alt="" role="presentation" />
+            <h4>Email Campaigns</h4>
+            <p>Do you want to send targeted email campaigns? I can build, test, segment and deploy HTML email campaigns for
+              you. I can also track the analytics that you value&nbsp;most.</p>
+          </div>
+        </div>
       </Section>
       <Section background="white"></Section>
       <Section background="black"></Section>
---
+++ src/components/IndexBanner/IndexBanner.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+
+import './IndexBanner.scss';
+
+import * as THREE from "three";
+
+class IndexBanner extends Component {
+  componentDidMount() {
+    var camera,
+    scene,
+    renderer,
+    particles,
+    particle,
+    material,
+    particleCount,
+    points,
+    texture;
+
+    var xSpeed, ySpeed;
+    xSpeed = 0.0005;
+    ySpeed = 0.0005;
+
+    var winWidth, winHeight;
+    var parentContainer = document.getElementById('mainCanvas');
+
+    winWidth = parentContainer.clientWidth;
+    winHeight = parentContainer.clientHeight;
+
+    // Resize
+    window.addEventListener('resize', onWindowResize, false);
+
+    function onWindowResize() {
+      camera.aspect = parentContainer.clientWidth / parentContainer.clientHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(parentContainer.clientWidth, parentContainer.clientHeight);
+    }
+
+    function init() {
+      scene = new THREE.Scene();
+      scene.fog = new THREE.FogExp2('#222', 0.001);
+
+      camera = new THREE.PerspectiveCamera(75, winWidth / winHeight, 1, 1000);
+      camera.position.z = 1000;
+
+      material = new THREE.PointsMaterial({
+        color: '#477979',
+        size: 3,
+        transparent: true,
+        blending: THREE.AdditiveBlending
+      });
+
+      particleCount = 12500;
+      particles = new THREE.Geometry();
+
+      for (var i = 0; i < particleCount; i++) {
+        var px = Math.random() * 2000 - 1000;
+        var py = Math.random() * 2000 - 1000;
+        var pz = Math.random() * 2000 - 1000;
+        particle = new THREE.Vector3(px, py, pz);
+        particle.velocity = new THREE.Vector3(0, 0, 0);
+        particles.vertices.push(particle);
+      }
+
+      points = new THREE.Points(particles, material);
+      points.sortParticles = true;
+      scene.add(points);
+
+      renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(winWidth, winHeight);
+      renderer.setClearColor('#181818', 1);
+      document.getElementById('canvas').appendChild(renderer.domElement);
+    }
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      scene.rotation.y += xSpeed;
+
+      var i = particleCount;
+      while (i--) {
+        var particle = particles.vertices[i];
+
+        if (particle.y > 1000) {
+          particle.y = -1000;
+          particle.velocity.y = Math.random();
+        }
+        particle.velocity.y += Math.random() * ySpeed;
+
+        particle.add(particle.velocity);
+      }
+      points.geometry.verticesNeedUpdate = true;
+
+      render();
+    }
+
+    function render() {
+      camera.lookAt(scene.position);
+      renderer.render(scene, camera);
+    }
+
+    init();
+    animate();
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.frameId);
+    this.mount.removeChild(this.renderer.domElement);
+  }
+
+  render() {
+    return (
+      <div id="mainCanvas">
+        <div id="index-title">
+          <div class="small-12 column">
+            <h1 data-aos="fade-down" data-aos-delay="600">Corey&nbsp;Noble</h1>
+            <h2 data-aos="fade-up" data-aos-delay="1200"><em>Web&nbsp;Application&nbsp;Developer</em></h2>
+          </div>
+        </div>
+        <div id="canvas"></div>
+      </div>
+    );
+  }
+}
+
+export default IndexBanner;
