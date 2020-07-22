2f7f7d17b2a17aa5cdba51143b8545e34fe447d6
Corey Noble <coreynoble@hotmail.ca>
2020-07-22 12:27:18 -0400

    attempting to resolve mobile navigation toggle, working without transition after trying to implement Jquery .slideToggle() in es6 JS

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from "react-router-dom";
 
 import classes from './Navigation.module.scss';
@@ -6,17 +6,116 @@ import classes from './Navigation.module.scss';
 import GitHubLogo from '../../assets/img/github-icon.png';
 import LinkedInLogo from '../../assets/img/linkedin-icon.png';
 
-const navigation = props => {
-  let action = 1;
+const Navigation = props => {
+  const navButton = useRef();
+  const homeLink = useRef();
 
-  function viewSomething() {
-    if (action === 1) {
-      document.getElementById('pull').innerHTML = '&nbsp;';
-      action = 2;
+  let shouldOpenNav = true;
+
+  /**
+  * SlideUp
+  *
+  * @param {HTMLElement} element
+  * @param {Number} duration
+  * @returns {Promise<boolean>}
+  */
+  const slideUp = (element, duration = 500) => {
+    return new Promise(function (resolve, reject) {
+      element.style.height = element.offsetHeight + 'px';
+      element.style.transitionProperty = `height, margin, padding`;
+      element.style.transitionDuration = duration + 'ms';
+      element.offsetHeight;
+      element.style.overflow = 'hidden';
+      element.style.height = 0;
+      element.style.paddingTop = 0;
+      element.style.paddingBottom = 0;
+      element.style.marginTop = 0;
+      element.style.marginBottom = 0;
+
+      window.setTimeout(function () {
+          element.style.display = 'none';
+          element.style.removeProperty('height');
+          element.style.removeProperty('padding-top');
+          element.style.removeProperty('padding-bottom');
+          element.style.removeProperty('margin-top');
+          element.style.removeProperty('margin-bottom');
+          element.style.removeProperty('overflow');
+          element.style.removeProperty('transition-duration');
+          element.style.removeProperty('transition-property');
+          resolve(false);
+      }, duration)
+    })
+  };
+
+  /**
+  * SlideDown
+  *
+  * @param {HTMLElement} element
+  * @param {Number} duration
+  * @returns {Promise<boolean>}
+  */
+  const slideDown = (element, duration = 500) => {
+    return new Promise(function (resolve, reject) {
+      element.style.removeProperty('display');
+      let display = window.getComputedStyle(element).display;
+
+      if (display === 'none') 
+          display = 'block';
+
+      element.style.display = display;
+      let height = element.offsetHeight;
+      element.style.overflow = 'hidden';
+      element.style.height = 0;
+      element.style.paddingTop = 0;
+      element.style.paddingBottom = 0;
+      element.style.marginTop = 0;
+      element.style.marginBottom = 0;
+      element.offsetHeight;
+      element.style.transitionProperty = `height, margin, padding`;
+      element.style.transitionDuration = duration + 'ms';
+      element.style.height = height + 'px';
+      element.style.removeProperty('padding-top');
+      element.style.removeProperty('padding-bottom');
+      element.style.removeProperty('margin-top');
+      element.style.removeProperty('margin-bottom');
+
+      window.setTimeout(function () {
+          element.style.removeProperty('height');
+          element.style.removeProperty('overflow');
+          element.style.removeProperty('transition-duration');
+          element.style.removeProperty('transition-property');
+      }, duration)
+    })
+  };
+
+  /**
+  * SlideToggle
+  *
+  * @param {HTMLElement} element
+  * @param {Number} duration
+  * @returns {Promise<boolean>}
+  */
+  const slideToggle = (element, duration = 500) => {
+    if (window.getComputedStyle(element).display === 'none') {
+        return slideDown(element, duration);
     } else {
-      document.getElementById('pull').innerHTML = '<strong><span>&lsaquo;</span> corey-noble /<span>&rsaquo;</span></strong>';
-      action = 1;
+        return slideUp(element, duration);
     }
+  };
+
+  const viewSomething = () => {
+    if (shouldOpenNav) {
+      navButton.current.innerHTML = '&nbsp;';
+      shouldOpenNav = false;
+    } else {
+      navButton.current.innerHTML = 
+      '<strong><span>&lsaquo;</span> corey-noble /<span>&rsaquo;</span></strong>';
+      shouldOpenNav = true;
+    }
+
+    const navList = document.querySelector('nav').childNodes[0];
+
+    slideToggle(navList);
   }
 
   return (
@@ -30,6 +129,8 @@ const navigation = props => {
               className={classes.NavHomeLink}
               to={`${process.env.PUBLIC_URL}/`}
               title="Visit the home page"
+              onClick={viewSomething}
+              ref={homeLink}
             >
               <strong>&lsaquo; corey-noble /&rsaquo;</strong>
             </Link>
@@ -38,6 +139,7 @@ const navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/portfolio`}
               title="View my portfolio"
+              onClick={viewSomething}
             >
               Portfolio
             </Link>
@@ -46,6 +148,7 @@ const navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/about`}
               title="Learn more about me"
+              onClick={viewSomething}
             >
               About
             </Link>
@@ -54,6 +157,7 @@ const navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/blog`}
               title="Read my blog"
+              onClick={viewSomething}
             >
               Blog
             </Link>
@@ -62,6 +166,7 @@ const navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/contact`}
               title="View my contact page"
+              onClick={viewSomething}
             >
               Contact
             </Link>
@@ -87,7 +192,14 @@ const navigation = props => {
             </a>
           </li>
         </ul>
-        <a href="#" className={classes.Pull} onClick={() => {viewSomething()}} title="Toggle the navigation menu" aria-label="Navigation Menu">
+        <a 
+          href="#" 
+          className={classes.Pull} 
+          title="Toggle the navigation menu" 
+          aria-label="Navigation Menu"
+          onClick={viewSomething}
+          ref={navButton}
+        >
           <strong><span>&lsaquo;</span> corey-noble /<span>&rsaquo;</span></strong>
         </a>
       </nav>
@@ -95,4 +207,4 @@ const navigation = props => {
   );
 };
 
-export default navigation;
+export default Navigation;
