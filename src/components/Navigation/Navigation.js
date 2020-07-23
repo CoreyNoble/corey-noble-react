e3f43a726eb341127fdfbe9376c6ac96c405ffa2
Corey Noble <coreynoble@hotmail.ca>
2020-07-22 20:08:27 -0400

    Mobile Navigation is functioning

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,127 +9,53 @@ import LinkedInLogo from '../../assets/img/linkedin-icon.png';
 const Navigation = props => {
   const navButton = useRef();
   const homeLink = useRef();
+  const mobileResolution = window.matchMedia("(max-width: 940px)");
+
+  let navStyle;
+
+  if (mobileResolution.matches) {
+    navStyle = {
+      transform: 'translateY(-16.9em)'
+    };
+  } else {
+    navStyle = {
+      transform: 'translateY(0)'
+    };
+  }
 
   let shouldOpenNav = true;
 
-  /**
-  * SlideUp
-  *
-  * @param {HTMLElement} element
-  * @param {Number} duration
-  * @returns {Promise<boolean>}
-  */
-  const slideUp = (element, duration = 500) => {
-    return new Promise(function (resolve, reject) {
-      element.style.height = element.offsetHeight + 'px';
-      element.style.transitionProperty = `height, margin, padding`;
-      element.style.transitionDuration = duration + 'ms';
-      element.offsetHeight;
-      element.style.overflow = 'hidden';
-      element.style.height = 0;
-      element.style.paddingTop = 0;
-      element.style.paddingBottom = 0;
-      element.style.marginTop = 0;
-      element.style.marginBottom = 0;
+  const toggleMobileNav = () => {
+    const nav = document.querySelector('nav');
 
-      window.setTimeout(function () {
-          element.style.display = 'none';
-          element.style.removeProperty('height');
-          element.style.removeProperty('padding-top');
-          element.style.removeProperty('padding-bottom');
-          element.style.removeProperty('margin-top');
-          element.style.removeProperty('margin-bottom');
-          element.style.removeProperty('overflow');
-          element.style.removeProperty('transition-duration');
-          element.style.removeProperty('transition-property');
-          resolve(false);
-      }, duration)
-    })
-  };
-
-  /**
-  * SlideDown
-  *
-  * @param {HTMLElement} element
-  * @param {Number} duration
-  * @returns {Promise<boolean>}
-  */
-  const slideDown = (element, duration = 500) => {
-    return new Promise(function (resolve, reject) {
-      element.style.removeProperty('display');
-      let display = window.getComputedStyle(element).display;
-
-      if (display === 'none') 
-          display = 'block';
-
-      element.style.display = display;
-      let height = element.offsetHeight;
-      element.style.overflow = 'hidden';
-      element.style.height = 0;
-      element.style.paddingTop = 0;
-      element.style.paddingBottom = 0;
-      element.style.marginTop = 0;
-      element.style.marginBottom = 0;
-      element.offsetHeight;
-      element.style.transitionProperty = `height, margin, padding`;
-      element.style.transitionDuration = duration + 'ms';
-      element.style.height = height + 'px';
-      element.style.removeProperty('padding-top');
-      element.style.removeProperty('padding-bottom');
-      element.style.removeProperty('margin-top');
-      element.style.removeProperty('margin-bottom');
-
-      window.setTimeout(function () {
-          element.style.removeProperty('height');
-          element.style.removeProperty('overflow');
-          element.style.removeProperty('transition-duration');
-          element.style.removeProperty('transition-property');
-      }, duration)
-    })
-  };
-
-  /**
-  * SlideToggle
-  *
-  * @param {HTMLElement} element
-  * @param {Number} duration
-  * @returns {Promise<boolean>}
-  */
-  const slideToggle = (element, duration = 500) => {
-    if (window.getComputedStyle(element).display === 'none') {
-        return slideDown(element, duration);
-    } else {
-        return slideUp(element, duration);
-    }
-  };
-
-  const viewSomething = () => {
     if (shouldOpenNav) {
+      nav.style.removeProperty('transform');
       navButton.current.innerHTML = '&nbsp;';
       shouldOpenNav = false;
+      homeLink.current.focus();
     } else {
+      nav.style.transform = 'translateY(-16.9em)';
       navButton.current.innerHTML = 
       '<strong><span>&lsaquo;</span> corey-noble /<span>&rsaquo;</span></strong>';
       shouldOpenNav = true;
     }
-
-    const navList = document.querySelector('nav').childNodes[0];
-
-    slideToggle(navList);
-  }
+  };
 
   return (
     <React.Fragment>
       <a href="#main" className={classes.SkipMain}>Skip to main content</a>
 
-      <nav className={classes.Nav}>
+      <nav 
+        className={classes.Nav}
+        style={navStyle}
+      >
         <ul>
           <li className={classes.NavIndex}>
             <Link 
               className={classes.NavHomeLink}
               to={`${process.env.PUBLIC_URL}/`}
               title="Visit the home page"
-              onClick={viewSomething}
+              onClick={mobileResolution.matches ? toggleMobileNav : ''}
               ref={homeLink}
             >
               <strong>&lsaquo; corey-noble /&rsaquo;</strong>
@@ -139,7 +65,7 @@ const Navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/portfolio`}
               title="View my portfolio"
-              onClick={viewSomething}
+              onClick={mobileResolution.matches ? toggleMobileNav : ''}
             >
               Portfolio
             </Link>
@@ -148,7 +74,7 @@ const Navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/about`}
               title="Learn more about me"
-              onClick={viewSomething}
+              onClick={mobileResolution.matches ? toggleMobileNav : ''}
             >
               About
             </Link>
@@ -157,7 +83,7 @@ const Navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/blog`}
               title="Read my blog"
-              onClick={viewSomething}
+              onClick={mobileResolution.matches ? toggleMobileNav : ''}
             >
               Blog
             </Link>
@@ -166,7 +92,7 @@ const Navigation = props => {
             <Link 
               to={`${process.env.PUBLIC_URL}/contact`}
               title="View my contact page"
-              onClick={viewSomething}
+              onClick={mobileResolution.matches ? toggleMobileNav : ''}
             >
               Contact
             </Link>
@@ -197,7 +123,7 @@ const Navigation = props => {
           className={classes.Pull} 
           title="Toggle the navigation menu" 
           aria-label="Navigation Menu"
-          onClick={viewSomething}
+          onClick={mobileResolution.matches ? toggleMobileNav : ''}
           ref={navButton}
         >
           <strong><span>&lsaquo;</span> corey-noble /<span>&rsaquo;</span></strong>
