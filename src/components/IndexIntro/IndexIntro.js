33c9c05a8ef17c65d8bcd668af9f0267c48ee063
Corey Noble <coreynoble@hotmail.ca>
2020-07-23 10:49:16 -0400

    optimise IndexIntro Markup and Styling

--- src/components/IndexIntro/IndexIntro.js
+++ src/components/IndexIntro/IndexIntro.js
@@ -7,32 +7,38 @@ import classes from './IndexIntro.module.scss';
 
 import Section from '../../containers/Section/Section';
 
-import CoreyNobleIndexPortrait from '../../assets/img/Corey-Noble-Index-Portrait.png'
 import GitHubLogo from '../../assets/img/github-icon.png';
 import LinkedInLogo from '../../assets/img/linkedin-icon.png';
+import CoreyNobleIndexPortrait from '../../assets/img/Corey-Noble-Index-Portrait.png'
 
-const indexIntro = props => {
+const indexIntro = () => {
   return (
     <Section background="black">
-      <Row className={`${classes.AboutMe}`}>
+      <Row className={classes.AboutMe}>
         <Column small={12} large={7}>
           <h3>Hi! My name is <strong>Corey&nbsp;Noble</strong>,</h3>
           <br />
-          <p className={`${classes.h5} h5`}>I am a <strong>Web&nbsp;Application&nbsp;Developer</strong> currently employed by <a
+          <p className={`${classes.IntroParagraph} h5`}>I am a <strong>Web&nbsp;Application&nbsp;Developer</strong> currently employed by <a
               className="evertz-text" href="https://evertz.com/" title="Visit the Evertz Microsystems Website" target="_blank"
               rel="noopener noreferrer"><strong>Evertz&nbsp;Microsystems&nbsp;Ltd.</strong></a>, A global leader in
             broadcast solutions with headquarters based in the beautiful city of
             Burlington,&nbsp;ON&nbsp;&#8209;&nbsp;Canada.</p>
-          <p className={`${classes.h5} h5`}>Welcome to my website! A portfolio that showcases some of my work, as well as the skills and
+          <p className={`${classes.IntroParagraph} h5`}>Welcome to my website! A portfolio that showcases some of my work, as well as the skills and
             experience that I bring to the&nbsp;table. If you like what you see and would like to reach out, please <Link
               to={`${process.env.PUBLIC_URL}/contact`} title="Contact Me">contact&nbsp;me</Link>.</p>
           <span className={classes.Socials}>
-            <a className={classes.GithubLink} href="https://github.com/CoreyNoble" target="_blank" rel="noopener noreferrer"
-              title="View my GitHub">
+            <a 
+              href="https://github.com/CoreyNoble"
+              target="_blank" rel="noopener noreferrer"
+              title="View my GitHub"
+            >
               <img src={GitHubLogo} alt="GitHub" />
             </a>
-            <a className={classes.LinkedInLink} href="https://ca.linkedin.com/in/corey-noble" target="_blank" rel="noopener noreferrer"
-              title="View my LinkedIn">
+            <a 
+              href="https://ca.linkedin.com/in/corey-noble"
+              target="_blank" rel="noopener noreferrer"
+              title="View my LinkedIn"
+            >
               <img src={LinkedInLogo} alt="LinkedIn" />
             </a>
           </span>
